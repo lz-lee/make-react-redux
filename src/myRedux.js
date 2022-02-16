baa68ae07bc81c6a9f1ed2fa75dde3bf8ad51012
liliangzhi <296452017@qq.com>
2022-02-16 14:24:55 +0800

    fix: applymiddleware dispatch 两次赋值

--- src/myRedux.js
+++ src/myRedux.js
@@ -32,16 +32,16 @@ export function createStore(reducer, enhancer) {
 }
 
 /**
- * @param creators 
+ * @param creators
  * function changeColor (...args) {
  *  return {
  *    type: 'CHANGE_COLOR,
  *    payload: args
  *  }
  * }
- * 
+ *
  * @param dispatch
- * 
+ *
  * @return {actionName: (...args) => dispatch(creator(...args))}
  */
 export function bindActionCreators(creators, dispatch) {
@@ -49,6 +49,9 @@ export function bindActionCreators(creators, dispatch) {
   // Object.keys(creators).forEach(v => {
   //   bound[v] = bindActionCreator(creators[v], dispatch)
   // })
+  if (typeof creators === 'function') {
+    return bindActionCreator(creators, dispatch);
+  }
   return Object.keys(creators).reduce((ret, item) => {
     ret[item] = bindActionCreator(creators[item], dispatch)
     return ret
@@ -66,8 +69,9 @@ const bindActionCreator = (creator, dispatch) => {
 export function applyMiddleware(...middlewares) {
   return (createStore) => (...args) => {
     const store = createStore(...args)
-    let dispatch = createStore.dispatch
-
+    // 希望在 middleware 链被构建的时候，dispatch 不应该被调用，否则抛错。因为中间件里的 dispatch 是经过增强后的
+    // 即后续实际会用第二次赋值的 dispatch
+    let dispatch = () => {};
     const midApi = {
       getState: store.getState,
       dispatch: (...args) => dispatch(...args)
@@ -75,6 +79,9 @@ export function applyMiddleware(...middlewares) {
     // 单个中间件
     // dispatch = middleware(midApi)(store.dispatch)
     const middlewareChain = middlewares.map(middleware => middleware(midApi))
+    // middlewareChain => [ (next) => action => {...}, xxx, xxx ]
+    // 这里 store.dispatch 即 compose 里 (...args) => ret(item(...args)) 这里的 args
+    // 修改了 dispatch
     dispatch = compose(...middlewareChain)(store.dispatch)
     return {
       ...store,
@@ -82,20 +89,9 @@ export function applyMiddleware(...middlewares) {
     }
   }
 }
-
-export const thunk = ({dispatch, getState}) => (next) => (action) => {
-  console.log(action, typeof action)
-  // 如果是函数，则执行函数，参数是dispatch 和getState
-  if (typeof action === 'function') {
-    return action(dispatch, getState)
-  }
-  // 默认使用原来的dispatch
-  return next(action)
-}
-
 /**
- * 
- * @param {fn1, fn2, fn3,...} middlewareChain 
+ *
+ * @param {fn1, fn2, fn3,...} middlewareChain
  * fn1(fn2(fn3))
  */
 export const compose = (...middlewareChain) => {
@@ -105,20 +101,37 @@ export const compose = (...middlewareChain) => {
   if (middlewareChain.length === 1) {
     return middlewareChain[0]
   }
-  
+
   return middlewareChain.reduce((ret, item) => (...args) => ret(item(...args)))
 }
 
+export const thunk = ({dispatch, getState}) => (next) => (action) => {
+  console.log(action, typeof action, '<<< thunk action');
+  console.log(next)
+  // 如果是函数，则执行函数，参数是dispatch 和getState
+  if (typeof action === 'function') {
+    // 这里的 dispatch 是 const midApi = {
+    //   getState: store.getState,
+    //   dispatch: (...args) => dispatch(...args)
+    // }
+    // 这里  dispatch, 而 dispatch(...args) 调用这个的 dispatch ，是 dispatch = compose(...middlewareChain)(store.dispatch) 返回的 dispatch 修改的，因此当异步执行到的 action ，可以再次进入中间价里执行
+    return action(dispatch, getState)
+  }
+  // 下一个 thunk，如果还有别的 thunk，则传递到 next thunk， 如果没有则 next 即是 store.dispatch (默认使用原来的dispatch)， 即执行了 reducer
+  return next(action)
+}
+
+
 export const arrThunk = ({dispatch, getState}) => (next) => (action) => {
-  console.log(action, typeof action)
+  console.log(action, typeof action, '<<< arr thunk action')
   if (Array.isArray(action)) {
     action.forEach(v => dispatch(v))
   }
   // 如果是函数，则执行函数，参数是dispatch 和getState
-  // if (typeof action === 'function') {
-  //   return action(dispatch, getState)
-  // }
-  // 默认使用原来的dispatch
+  if (typeof action === 'function') {
+    return action(dispatch, getState)
+  }
+  // 下一个 thunk，如果没有 默认使用原来的dispatch
   return next(action)
 }
 
@@ -127,8 +140,8 @@ export const arrThunk = ({dispatch, getState}) => (next) => (action) => {
  * let addAction = createAction('ADD', 'count')
  * addAction(1)
  * 相当于 function addAction(id) {return {type: 'ADD', count: count}}
- * @param {*} type 
- * @param {*} argNames 
+ * @param {*} type
+ * @param {*} argNames
  */
 export const createAction = (type, ...argNames) => (...args) => {
   let action = {type}
@@ -146,8 +159,8 @@ export const createAction = (type, ...argNames) => (...args) => {
  *        return state
  *    }
  * })
- * @param {*} initState 
- * @param {*} handles 
+ * @param {*} initState
+ * @param {*} handles
  */
 export const createReducer = (initState, handles = {}) => (state = initState, action = {}) => {
   if (handles.hasOwnProperty(action.type)) {
